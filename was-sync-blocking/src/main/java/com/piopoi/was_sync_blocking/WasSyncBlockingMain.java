f9d31cc547b8900b217260fb59a4c0f788b01509
piopoi <kangsw1988@gmail.com>
2024-11-22 00:36:08 +0900

    set: - multi-module - build.gradle - logback.xml

---
+++ was-sync-blocking/src/main/java/com/piopoi/was_sync_blocking/WasSyncBlockingMain.java
@@ -0,0 +1,12 @@
+package com.piopoi.was_sync_blocking;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WasSyncBlockingMain {
+    private static final Logger logger = LoggerFactory.getLogger(WasSyncBlockingMain.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World!");
+    }
+}
