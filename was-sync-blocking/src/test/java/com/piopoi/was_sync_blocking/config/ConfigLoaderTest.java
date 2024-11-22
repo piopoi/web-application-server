2e8d03d4b2cb015dea479739728be059250680d8
piopoi <kangsw1988@gmail.com>
2024-11-22 15:17:40 +0900

    #1 feat: config.json 설정파일 로딩 기능 추가

---
+++ was-sync-blocking/src/test/java/com/piopoi/was_sync_blocking/config/ConfigLoaderTest.java
@@ -0,0 +1,24 @@
+package com.piopoi.was_sync_blocking.config;
+
+import static com.piopoi.was_sync_blocking.util.ReflectionTestUtils.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConfigLoaderTest {
+
+    @Test
+    void loadServerConfig() throws NoSuchFieldException, IllegalAccessException {
+        // when
+        ServerConfig serverConfig = ServerConfig.getInstance();
+
+        // then
+        int minPort = getInaccessibleFieldValue("MIN_PORT", Integer.class);
+        int maxPort = getInaccessibleFieldValue("MAX_PORT", Integer.class);
+        int minThreadPoolSize = getInaccessibleFieldValue("MIN_THREAD_POOL_SIZE", Integer.class);
+
+        assertThat(serverConfig).isNotNull();
+        assertThat(serverConfig.getPort()).isBetween(minPort, maxPort);
+        assertThat(serverConfig.getThreadPoolSize()).isGreaterThanOrEqualTo(minThreadPoolSize);
+    }
+}
