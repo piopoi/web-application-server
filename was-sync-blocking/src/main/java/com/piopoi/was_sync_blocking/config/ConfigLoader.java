86d522b8b7b283dd1ef73f3887a00694a79cf5f2
piopoi <kangsw1988@gmail.com>
2024-11-24 21:05:42 +0900

    feat: 동기/블로킹 방식의 서버 코어 모듈 개발 외.
    - 동기/블로킹 방식의 서버 코어 모듈 개발.
    - 서버 코어는 통합 테스트만 작성.
    - ConfigLoader를 ServerConfig에 종속되지 않도록 수정.
    - 기본 설정 포트 변경: 8000 -> 8080
    - 테스트 설정 포트 변경: 8001 -> 8081
    - README.md 내용 변경: 기능 구현 범위 작성

--- was-sync-blocking/src/main/java/com/piopoi/was_sync_blocking/config/ConfigLoader.java
+++ was-sync-blocking/src/main/java/com/piopoi/was_sync_blocking/config/ConfigLoader.java
@@ -8,14 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ConfigLoader {
-    public static ServerConfig loadServerConfig(String fileName) throws IOException {
+    public static <T> T loadConfig(Class<T> clazz, String fileName) throws IOException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
             if (inputStream == null) {
                 throw new IOException("Configuration file not found: " + fileName);
             }
             ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-            return objectMapper.readValue(inputStream, ServerConfig.class);
+            return objectMapper.readValue(inputStream, clazz);
         } catch (IOException e) {
             log.error(e.getMessage());
             throw e;
